33c4db33f0ef50a0fa692033b14213bdc095fd21
Shahroz Asif <shahrozasif011@gmail.com>
2023-05-10 20:48:52 +0500

    task(pattern): complete pattern

--- tasks/pattern.cpp
+++ tasks/pattern.cpp
@@ -7,14 +7,17 @@ void drawCPP(int logoFactor) {
   for (int i = 0; i < logoSize; ++i) {
     int letterCount { i == 0 || i == (logoSize - 1) ? logoFactor : 1 };
     int plusCount { i == middleIndex ? logoSize : 1 };
-    int spacesBefore { i < middleIndex ? logoFactor - i - 1 : i - middleIndex };
-    int spacesAfter { logoSize - letterCount - spacesBefore + 1 };
+    int spacesBeforeLetter { i < middleIndex ? logoFactor - i - 1 : i - middleIndex };
+    int spacesAfterLetter { logoSize - letterCount - spacesBeforeLetter + 1 };
+    int spacesAfterPlus { i == middleIndex ? 1 : 2 * logoFactor - 1 };
 
-    if (i != middleIndex) spacesAfter += middleIndex;
+    if (i != middleIndex) spacesAfterLetter += middleIndex;
 
-    for (int j = 0; j < spacesBefore; ++j) std::cout << " ";
+    for (int j = 0; j < spacesBeforeLetter; ++j) std::cout << " ";
     for (int j = 0; j < letterCount; ++j) std::cout << "C";
-    for (int j = 0; j < spacesAfter + 1; ++j) std::cout << " ";
+    for (int j = 0; j < spacesAfterLetter; ++j) std::cout << " ";
+    for (int j = 0; j < plusCount; ++j) std::cout << "+";
+    for (int j = 0; j < spacesAfterPlus; ++j) std::cout << " ";
     for (int j = 0; j < plusCount; ++j) std::cout << "+";
 
     std::cout << std::endl;
